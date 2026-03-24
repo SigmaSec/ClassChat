dd2f4c596426c6cf84a64cd9280ba7fa1ce8428c
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-24 09:44:36 -0400

    server finished I believe

--- Server.py
+++ Server.py
@@ -22,18 +22,19 @@ def handle_client(client_socket):
     while True:
         data = client_socket.recv(1024).decode('utf-8')
         message = json.loads(data)
-        recipient = message["reciever"]
-        if recipient in users:
-            users[recipient].send(f"This room is private(?)".encode('utf-8'))
-        else:
-            client_socket.send("User not found".encode('utf-8'))
+        if message["status"] == "private":
+            recipient = message["receiver"]
+            if recipient in users:
+                users[recipient].send(f"{message['sender']}: {message['text']}".encode('utf-8'))
+            else:
+                client_socket.send("User not found".encode('utf-8'))
         elif message["status"] == "group":
             room_name = message["receiver"]
             if room_name in rooms:
                 for member in rooms[room_name]:
                     users[member].send(f"{message['sender']} [{room_name}]: {message['text']}".encode('utf-8'))
             else:
-                client_socket.send("Room unable to create")
+                client_socket.send("Room unable to create".encode('utf-8'))
         elif message["status"] == "create":
             room_name = message["receiver"]
             if room_name not in rooms:
@@ -61,3 +62,6 @@ def start_server():
         client_socket, address = server_socket.accept()
         thread = threading.Thread(target=handle_client, args=(client_socket,))
         thread.start()
+
+if __name__ == "__Main__":
+    start_server()
