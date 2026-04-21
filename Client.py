c7b1b8821f9e9bb9c93f41b8116179ade2554f0a
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-04-21 09:53:37 -0400

    try execpt blocks added

--- Client.py
+++ Client.py
@@ -4,54 +4,60 @@ import json
 
 def recieve_messages(client_socket):
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        
-        print(message)
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print("Disconnected from server")
+            break
 def send_messages(client_socket, username):
     while True:
-        message = input()
-        parts = message.split()
+        try:
+            message = input()
+            parts = message.split()
 
-        if parts[0] == "/private":
-            data = {
-                "status": "private",
-                "sender": username,
-                "receiver": parts[1],
-                "text": " ".join(parts[2:])
-            }
-            client_socket.send(json.dumps(data).encode('utf-8'))
-        elif parts[0] == "/group":
-            data = {
-                "status": "group",
-                "sender": username,
-                "receiver": parts[1],
-                "text" : " ".join(parts[2:])
-            }
-            client_socket.send(json.dumps(data).encode('utf-8'))
-        elif parts[0] == "/create":
-            data = {
-                "status": "create",
-                "sender": username,
-                "receiver": parts[1],
-                "text": " ".join(parts[2:])
-            }
-            client_socket.send(json.dumps(data).encode('utf-8'))
-        elif parts[0] == "/join":
-            data = {
-                "status": "join",
-                "sender": username,
-                "receiver": parts[1],
-                "text":" ".join(parts[2:])
-            }
-            client_socket.send(json.dumps(data).encode('utf-8'))
-        elif parts[0] == "/quit":
-            data = {
-                "status": "quit",
-                "sender": username,
-                "receiver": "",
-                "text": ""
-            }
-            client_socket.send(json.dumps(data).encode('utf-8'))
+            if parts[0] == "/private":
+                data = {
+                    "status": "private",
+                    "sender": username,
+                    "receiver": parts[1],
+                    "text": " ".join(parts[2:])
+                }
+                client_socket.send(json.dumps(data).encode('utf-8'))
+            elif parts[0] == "/group":
+                data = {
+                    "status": "group",
+                    "sender": username,
+                    "receiver": parts[1],
+                    "text" : " ".join(parts[2:])
+                }
+                client_socket.send(json.dumps(data).encode('utf-8'))
+            elif parts[0] == "/create":
+                data = {
+                    "status": "create",
+                    "sender": username,
+                    "receiver": parts[1],
+                    "text": " ".join(parts[2:])
+                }
+                client_socket.send(json.dumps(data).encode('utf-8'))
+            elif parts[0] == "/join":
+                data = {
+                    "status": "join",
+                    "sender": username,
+                    "receiver": parts[1],
+                    "text":" ".join(parts[2:])
+                }
+                client_socket.send(json.dumps(data).encode('utf-8'))
+            elif parts[0] == "/quit":
+                data = {
+                    "status": "quit",
+                    "sender": username,
+                    "receiver": "",
+                    "text": ""
+                }
+                client_socket.send(json.dumps(data).encode('utf-8'))
+        except:
+            print("Error sending message")
             break
 def start_client():
     host = "127.0.0.1"
