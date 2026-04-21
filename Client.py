843d6bffe45bbb072741b73e41e0984df712360f
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-04-20 21:25:08 -0400

    yerp

--- Client.py
+++ Client.py
@@ -1,5 +1,16 @@
 import socket
 import threading
 import json
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
+
+def recieve_messages(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        print(message)
+def send_messages(client_socket, username):
+    while True:
+        message = input()
+
+
+
+#client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client_socket.connect((host, port))
